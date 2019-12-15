2c9792c1423a9c22b5ada060914020a32b7bb8e3
Nicholas Tran <phikhant@uci.edu>
2019-12-15 13:47:28 -0800

    Updated error message. Add match info

--- summoner/views.py
+++ summoner/views.py
@@ -24,18 +24,25 @@ def home(request):
     summoner_name = request.GET.get('summoner_name')
     region = request.GET.get("region")
 
+    # Get Api key
     cur_path = os.getcwd()
     file = open(os.path.join(cur_path, '_info', 'api.txt'), "r") #path to api key
     APIKey = file.read()
 
     watcher = RiotWatcher(APIKey)
-    summoner_data = watcher.summoner.by_name(region, summoner_name)
+    try:
+        summoner_data = watcher.summoner.by_name(region, summoner_name)
+    except ApiError as e:
+        status_code = e.response.status_code
+        
+        return render(request, "summoner/error.html", {"summonerData": {"name": summoner_name}, "status": status_code})
 
     # if summoner not found
     if "status" in summoner_data:
         return render(request, "summoner/error.html", {"summonerData": {"name": summoner_name}})
 
     match_list = watcher.match.matchlist_by_account(region, summoner_data["accountId"])
+    league_data = watcher.league.by_summoner(region, summoner_data["id"])
 
     #Static content
     version = watcher.data_dragon.versions_for_region(region)['v']
@@ -46,8 +53,9 @@ def home(request):
 
     # runes = watcher.data_dragon.runes(version) not working??
 
-    context = {"summonerData": summoner_data, "matches": match_list, "champions": champions,
-                "items": items, "profile_icons": profile_icons, "summoner_spells": summoner_spells}
+    context = {"summonerData": summoner_data, "matches": match_list['matches'], "leagues": league_data, 
+                "champions": champions, "items": items, "profile_icons": profile_icons, 
+                "summoner_spells": summoner_spells}
 
     return render(request, 'summoner/home.html', context)
 
