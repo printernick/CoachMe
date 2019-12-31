b4c773c79031a5da022bdcded7609f5373300485
Nicholas Tran <phikhant@uci.edu>
2019-12-30 21:08:50 -0800

    Work towards adding champion icons

--- summoner/scripts/summoner/riotRequestFunctions.py
+++ summoner/scripts/summoner/riotRequestFunctions.py
@@ -9,12 +9,6 @@ def requestSummonerData(region: str, summonerName: str, APIKey: str):
     response = requests.get(URL)
     return response.json()
 
-# def requestEntriesBySummoner(region:str, encryptedSummonerId: str, APIKey: str):
-#     # URL = f"https://{region}.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={APIKey}"
-#     URL = "adsf"
-#     response = requests.get(URL)
-#     return response.json()
-
 def requestMatchlist(region: str, encryptedAccountId: str, APIKey: str):
     '''
     region: RU, KR, BR1, OC1, JP1, NA1, EUN1, EUW1, TR1, LA1, LA2
@@ -22,5 +16,10 @@ def requestMatchlist(region: str, encryptedAccountId: str, APIKey: str):
     '''
 
     URL = f"https://{region}.api.riotgames.com/lol/match/v4/matchlists/by-account/{encryptedAccountId}?api_key={APIKey}"
+    response = requests.get(URL)
+    return response.json()
+
+def getStaticChampionInfo(championId: str):
+    URL = f"http://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/en_gb/v1/champions/{championId}.json"
     response = requests.get(URL)
     return response.json()
--- summoner/views.py
+++ summoner/views.py
@@ -4,21 +4,6 @@ import os
 
 from .scripts.summoner import riotRequestFunctions as riot
 from riotwatcher import RiotWatcher, ApiError
-posts = [
-    {
-        'author': 'Oogie Boogie',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'November 20, 2019'
-    },
-    {
-        'author': 'Oogie Troogie',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'November 30, 2019'
-    },
-
-]
 
 def home(request):
     summoner_name = request.GET.get('summoner_name')
@@ -40,21 +25,35 @@ def home(request):
     if "status" in summoner_data:
         return render(request, "summoner/error.html", {"summonerData": {"name": summoner_name}})
 
-    match_list = watcher.match.matchlist_by_account(region, summoner_data["accountId"])
+    match_list = watcher.match.matchlist_by_account(region, summoner_data["accountId"])['matches']
+    matches = {}
+    champion_ids = {}
+    for match in match_list[:min(25, len(match_list))]:
+        match_by_id = watcher.match.by_id(region, match["gameId"])
+
+        # 0th index = blue team, 1st index = red team
+        for ban in zip(match_by_id['teams'][0]['bans'], match_by_id['teams'][1]['bans']):
+            for i in range(len(ban)):
+                id = str(ban[i]['championId'])
+                if id not in champion_ids:
+                    champion_json = riot.getStaticChampionInfo(id)
+                    champion_ids[id] = champion_json
+        
+
     league_data = watcher.league.by_summoner(region, summoner_data["id"])
 
     #Static content
     version = watcher.data_dragon.versions_for_region(region)['v']
-    champions = watcher.data_dragon.champions(version)
-    items = watcher.data_dragon.items(version)
-    profile_icons = watcher.data_dragon.profile_icons(version)
-    summoner_spells = watcher.data_dragon.summoner_spells(version)
+    champions = watcher.data_dragon.champions(version)['data']
+    items = watcher.data_dragon.items(version)['data']
+    profile_icons = watcher.data_dragon.profile_icons(version)['data']
+    summoner_spells = watcher.data_dragon.summoner_spells(version)['data']
 
     # runes = watcher.data_dragon.runes(version) not working??
 
-    context = {"summonerData": summoner_data, "matches": match_list['matches'], "leagues": league_data, 
+    context = {"summonerData": summoner_data, "matches": match_list, "leagues": league_data, 
                 "champions": champions, "items": items, "profile_icons": profile_icons, 
-                "summoner_spells": summoner_spells}
+                "summoner_spells": summoner_spells, "champion_ids": champion_ids}
 
     return render(request, 'summoner/home.html', context)
 
